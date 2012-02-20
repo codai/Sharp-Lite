7793a4391600ed66bd0ea6d2467b0fb166f2499e
Dan Smith <sonomofo@gmail.com>
2012-02-20 18:38:50 -0500

    Added caching to template's NHibernate configuration initialization code

--- Template/TemplateSrc/app/TemplateSrc.NHibernateProvider/NHibernateInitializer.cs
+++ Template/TemplateSrc/app/TemplateSrc.NHibernateProvider/NHibernateInitializer.cs
@@ -10,19 +10,36 @@ namespace TemplateSrc.NHibernateProvider
     public class NHibernateInitializer
     {
         public static Configuration Initialize() {
-            Configuration configuration = new Configuration();
 
-            configuration
-                .Proxy(p => p.ProxyFactoryFactory<DefaultProxyFactoryFactory>())
-                .DataBaseIntegration(db => {
-                    db.ConnectionStringName = "TemplateSrcConnectionString";
-                    db.Dialect<MsSql2008Dialect>();
-                })
-                .AddAssembly(typeof(ActionConfirmation<>).Assembly)
-                .CurrentSessionContext<LazySessionContext>();
+            INHibernateConfigurationCache cache = new NHibernateConfigurationFileCache();
 
-            ConventionModelMapper mapper = new ConventionModelMapper();
-            mapper.WithConventions(configuration);
+            string[] mappingAssemblies = new string[] { 
+                typeof(ActionConfirmation<>).Assembly.GetName().Name
+            };
+            string configFile = null; //NHibernate.config is not used;
+            string configKey = "TemplateSrc";
+
+            Configuration configuration = cache.LoadConfiguration(configKey, configFile, mappingAssemblies);
+
+            if (configuration == null)
+            {
+                configuration = new Configuration();
+
+                configuration
+                    .Proxy(p => p.ProxyFactoryFactory<DefaultProxyFactoryFactory>())
+                    .DataBaseIntegration(db =>
+                    {
+                        db.ConnectionStringName = "TemplateSrcConnectionString";
+                        db.Dialect<MsSql2008Dialect>();
+                    })
+                    .AddAssembly(typeof(ActionConfirmation<>).Assembly)
+                    .CurrentSessionContext<LazySessionContext>();
+
+                ConventionModelMapper mapper = new ConventionModelMapper();
+                mapper.WithConventions(configuration);
+
+                cache.SaveConfiguration(configKey, configuration);
+            }
 
             return configuration;
         }
